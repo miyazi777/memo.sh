887ef09578164b880317b9676a52b529559f1206
miyazi777 <m71203@gmail.com>
2019-04-06 20:31:42 +0900

    first commit

---
+++ memo.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+function add() {
+  if [ -n "$1" ]; then
+    touch $MEMO_DIR/$1.md
+  fi
+}
+
+function list() {
+  ls -ls $MEMO_DIR
+}
+
+function edit() {
+  filename=$(ls -1 $MEMO_DIR | fzf +m)
+  if [ -n "$filename" ]; then
+    $EDITOR $MEMO_DIR/$filename
+  fi
+}
+
+function delete() {
+  filename=$(ls -1 $MEMO_DIR | fzf +m)
+  if [ -n "$filename" ]; then
+    rm $MEMO_DIR/$filename
+  fi
+}
+
+echo $2
+
+case $1 in
+  "add")
+    add $2
+    ;;
+  "list")
+    list
+    ;;
+  "edit")
+    edit $2
+    ;;
+  "delete")
+    delete
+    ;;
+  *)
+    ;;
+esac
+
+exit 0
+
